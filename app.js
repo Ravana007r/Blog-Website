1d31027d5e91df6a28b2b5429512d97e7c1a765d
Akash Kumar <ravana.007r@gmail.com>
2023-03-26 22:52:53 +0530

    database added

--- app.js
+++ app.js
@@ -4,22 +4,36 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require('lodash');
+const mongoose = require("mongoose");
+require('dotenv').config()
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+mongoose.connect(process.env.MONGODB);
+
 const app = express();
-let posts = [];
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req,res){
-  res.render("home", {homeContent:homeStartingContent, postedMessage:posts});
+// Schema
+const postScheme = {
+  title:String,
+  comment:String
+}
+ const allPosts = new mongoose.model('allPosts', postScheme);
 
+
+app.get("/", function(req,res){
+  allPosts.find({}, function(err,data){
+    if(!err)  res.render("home", {homeContent:homeStartingContent, postedMessage:data});
+  })
 })
 
 app.get("/contact",function(req,res){
@@ -32,37 +46,28 @@ app.get("/about", function(req,res){
 
 app.get("/compose", function(req,res){
   res.render("compose",{heading:"Compose"});
+  
 })
 
 app.post("/compose", function(req,res){
-  const data = {
-   titles: req.body.title,
-   postBody: req.body.postBody
-  };
+  
+  const post = new allPosts({
+      title: req.body.title,
+      comment: req.body.postBody
+  })
 
-  posts.push(data);
-  res.redirect("/");
-})
+  post.save(function(err){
+    if(!err) res.redirect("/"); 
+  });
+});
 
 app.get("/:postTitle", function(req,res){
-  const postRequest = lodash.lowerCase(req.params.postTitle);
+  const postRequest = req.params.postTitle;
   
-  posts.forEach(function(post){
-    const storeTItle =lodash.lowerCase(post.titles);
-    if (storeTItle=== postRequest) {
-      res.render("post", {postName: post.titles, content:post.postBody});
-    } 
-    // else {
-    //   res.send("<h1>Match Not Found</h1>");
-    // }
+  allPosts.findOne({_id:postRequest}, function(err,data){
+    if(!err) res.render("post", {postName:data.title, content:data.comment});
   })
 })
-
-
-
-
-
-
 
 
 
